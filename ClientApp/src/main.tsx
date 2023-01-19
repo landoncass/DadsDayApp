7abe09f916be0944e3c1635b996e55f450fcdd75
Landon Cassman <Landon.Cassman@gmail.com>
2023-01-19 18:43:06 -0500

    Initial commit

--- ClientApp/src/main.tsx
+++ ClientApp/src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { App } from './routes/App'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
