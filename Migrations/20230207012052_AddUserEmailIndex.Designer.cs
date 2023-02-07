ca614cb50805464fb0e38dbf647d0baa5b949d1d
Landon Cassman <Landon.Cassman@gmail.com>
2023-02-06 20:32:06 -0500

    Create a catch error so duplicate emails cannot create a user

--- Migrations/20230207012052_AddUserEmailIndex.Designer.cs
+++ Migrations/20230207012052_AddUserEmailIndex.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using DadsDayApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DadsDayApp.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230207012052_AddUserEmailIndex")]
+    partial class AddUserEmailIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
