9c5ccf94531a0da297725b2bde37f70e56cb7039
Landon Cassman <Landon.Cassman@gmail.com>
2023-03-10 16:25:00 -0500

    Adjusted days out

--- ClientApp/src/components/SingleDayOutFromList.tsx
+++ ClientApp/src/components/SingleDayOutFromList.tsx
@@ -8,32 +8,34 @@ export function SingleDayOutFromList({ dayOut }: { dayOut: DayOutType }) {
   const urlForShowingDayOut = `/daysout/${dayOut.id}`
   return (
     <li key={dayOut.id}>
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-2by2">
-            <img src={dayOut.photoURL} />
-          </figure>
-        </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-content">
-              <p className="title is-4">
-                <strong>Where:</strong> <Link to={urlForShowingDayOut}>{dayOut.location}</Link>
+      <div className="column">
+        <div className="card card-equal-height">
+          <div className="card-image">
+            <figure className="image is-2by2">
+              <img src={dayOut.photoURL} />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-content">
+                <p className="title is-4">
+                  <strong>Where:</strong> <Link to={urlForShowingDayOut}>{dayOut.location}</Link>
+                </p>
+              </div>
+            </div>
+            <div className="content"><p>
+            </p>
+              <p>
+                <strong>Address:</strong> {dayOut.address}
+              </p>
+              <p>
+                <strong>Description:</strong> {dayOut.description}
+              </p>
+              <p>
+                <Stars dayOut={dayOut} />
+                <strong>Reviews </strong> ({dayOut.reviews.length})
               </p>
             </div>
-          </div>
-          <div className="content"><p>
-          </p>
-            <p>
-              <strong>Address:</strong> {dayOut.address}
-            </p>
-            <p>
-              <strong>Description:</strong> {dayOut.description}
-            </p>
-            <p>
-              <Stars dayOut={dayOut} />
-              <strong>Reviews </strong> ({dayOut.reviews.length})
-            </p>
           </div>
         </div>
       </div>
